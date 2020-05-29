00436133a7c7e7f57aba504981c79f6fca73a443
Li-Kuo <55146208+Li-Kuo@users.noreply.github.com>
2020-05-29 10:56:53 +0800

    Upload a copy of a new javascript file

---
+++ helloWorld.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var http = require("http");
+http
+  .createSever(function (req, res) {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+    res.end("Hello World!");
+  })
+  .listen(3000);
+
+console.log("123");
